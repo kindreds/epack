2664fe9d8fd6aef11824844401cb026621405542
Cristian González <kazragore96@gmail.com>
2021-06-26 16:16:34 -0500

    Chunks optimizados 2

--- src/Sections/Servicios.js
+++ src/Sections/Servicios.js
@@ -1,25 +1,16 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import Image from 'next/image'
 // import PropTypes from 'prop-types'
 import { Button } from '@chakra-ui/button'
 import { useMediaQuery } from '@chakra-ui/media-query'
-import { useInView } from 'react-intersection-observer'
 import { Flex, Text, Heading, Box, SimpleGrid } from '@chakra-ui/layout'
 
 const Nosotros = (props) => {
   const [is670pxOrLess] = useMediaQuery('(max-height: 670px)')
 
-  const { ref, inView } = useInView()
-  const [load, setLoad] = useState(false)
-
-  useEffect(() => {
-    if (inView) return setLoad(true)
-  }, [inView])
-
   return (
     <>
       <Flex
-        ref={ref}
         mx="auto"
         mt="-1px"
         bg="gray.200"
@@ -31,66 +22,64 @@ const Nosotros = (props) => {
         // minH={{ base: '100vh', md: '700px' }}
         {...props}
       >
-        {load && (
-          <SimpleGrid
-            gap={{ md: 5 }}
-            alignItems="center"
-            justifyContent="center"
-            maxW={{ base: '85%', xl: 'container.xl' }}
-            templateColumns={{
-              base: 'minmax(0, 1fr)',
-              md: '400px 400px',
-              lg: 'minmax(0, 600px) 400px'
-            }}
-            templateRows={{
-              base: 'minmax(0, 250px)',
-              md: '400px',
-              lg: 'calc(100vh * 0.8)'
-            }}
+        <SimpleGrid
+          gap={{ md: 5 }}
+          alignItems="center"
+          justifyContent="center"
+          maxW={{ base: '85%', xl: 'container.xl' }}
+          templateColumns={{
+            base: 'minmax(0, 1fr)',
+            md: '400px 400px',
+            lg: 'minmax(0, 600px) 400px'
+          }}
+          templateRows={{
+            base: 'minmax(0, 250px)',
+            md: '400px',
+            lg: 'calc(100vh * 0.8)'
+          }}
+        >
+          <Box
+            h="full"
+            w="full"
+            mx="auto"
+            mb={is670pxOrLess ? 2 : 4}
+            pos="relative"
           >
-            <Box
-              h="full"
-              w="full"
-              mx="auto"
+            <Image src="/seriegrafia.png" layout="fill" objectFit="contain" />
+          </Box>
+          <Box>
+            <Heading
+              as="h1"
+              fontSize="3xl"
+              color="bgSecundary"
+              textAlign={{ base: 'center', md: 'left' }}
               mb={is670pxOrLess ? 2 : 4}
-              pos="relative"
             >
-              <Image src="/seriegrafia.png" layout="fill" objectFit="contain" />
-            </Box>
-            <Box>
-              <Heading
-                as="h1"
-                fontSize="3xl"
-                color="bgSecundary"
-                textAlign={{ base: 'center', md: 'left' }}
-                mb={is670pxOrLess ? 2 : 4}
-              >
-                SERVICIO DE SERIGRAFÍA
-              </Heading>
-              <Text
-                mb={4}
-                color="gray.700"
-                fontWeight="400"
-                textAlign="justify"
-                fontSize={{ base: 'lg', lg: 'xl' }}
-                lineHeight={is670pxOrLess ? 'short' : 'taller'}
-              >
-                EPACK, es la mejor opción en servicio de serigrafía, estampado,
-                tampografía y todo tipo de impresión, nuestro taller cuenta con
-                personal altamente calificado y solida experticia en el rubro.
-                Productos: Bolsas con asa, bolsas sin asa, Bolsas Doy Pack,
-                Bolsas con Asa Riñon, Stickers, etc
-              </Text>
-              <Button
-                size="lg"
-                colorScheme="secundary"
-                display={{ base: 'none', md: 'flex' }}
-              >
-                Contáctanos
-              </Button>
-            </Box>
-          </SimpleGrid>
-        )}
+              SERVICIO DE SERIGRAFÍA
+            </Heading>
+            <Text
+              mb={4}
+              color="gray.700"
+              fontWeight="400"
+              textAlign="justify"
+              fontSize={{ base: 'lg', lg: 'xl' }}
+              lineHeight={is670pxOrLess ? 'short' : 'taller'}
+            >
+              EPACK, es la mejor opción en servicio de serigrafía, estampado,
+              tampografía y todo tipo de impresión, nuestro taller cuenta con
+              personal altamente calificado y solida experticia en el rubro.
+              Productos: Bolsas con asa, bolsas sin asa, Bolsas Doy Pack, Bolsas
+              con Asa Riñon, Stickers, etc
+            </Text>
+            <Button
+              size="lg"
+              colorScheme="secundary"
+              display={{ base: 'none', md: 'flex' }}
+            >
+              Contáctanos
+            </Button>
+          </Box>
+        </SimpleGrid>
       </Flex>
     </>
   )
--- src/components/Slider/BlogSlider.js
+++ src/components/Slider/BlogSlider.js
@@ -1,11 +1,11 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import d from 'next/dynamic'
+import Image from 'next/image'
 
 import PropTypes from 'prop-types'
 import { Button } from '@chakra-ui/button'
 import { Flex, Heading, Box } from '@chakra-ui/layout'
 import { useMediaQuery } from '@chakra-ui/media-query'
-import { useInView } from 'react-intersection-observer'
 
 const ReactSlider = d(() => import('react-slidy'))
 
@@ -16,14 +16,8 @@ const BlogSlider = ({
   sizeHeading = '4xl',
   ...props
 }) => {
-  const { ref, inView } = useInView()
-  const [load, setLoad] = useState(false)
   const [actualSlide, setActualSlide] = useState(0)
   const [is670pxOrLess] = useMediaQuery('(max-height: 670px)')
-
-  useEffect(() => {
-    if (inView) return setLoad(true)
-  }, [inView])
 
   const isWhite = theme === 'white'
 
@@ -35,7 +29,6 @@ const BlogSlider = ({
     <Box w="full">
       <Flex
         {...props}
-        ref={ref}
         pb={100}
         flexDir="column"
         pos={{ md: 'relative' }}
@@ -56,12 +49,11 @@ const BlogSlider = ({
           h={{ base: '120px' }}
           w={{ base: '120px' }}
         >
-          {load && (
-            <img
-              style={{ height: '100%', width: '100%' }}
-              src="/icono-cicular.png"
-            />
-          )}
+          <Image
+            layout="fill"
+            style={{ height: '100%', width: '100%' }}
+            src="/icono-cicular.png"
+          />
         </Box>
         <Heading
           as="h1"
@@ -74,47 +66,45 @@ const BlogSlider = ({
         >
           {heading}
         </Heading>
-        {load && (
-          <Box
-            pos={{ md: 'absolute' }}
-            top={56}
-            left={0}
-            right={0}
-            maxW={{ md: '90%' }}
-            mx={{ md: 'auto' }}
+        <Box
+          pos={{ md: 'absolute' }}
+          top={56}
+          left={0}
+          right={0}
+          maxW={{ md: '90%' }}
+          mx={{ md: 'auto' }}
+        >
+          <ReactSlider
+            showArrows={!!0}
+            slide={actualSlide}
+            doAfterSlide={updateSlide}
           >
-            <ReactSlider
-              showArrows={!!0}
-              slide={actualSlide}
-              doAfterSlide={updateSlide}
-            >
-              {children}
-            </ReactSlider>
-            <Flex mt={5} ml={{ base: '10%', md: 0 }} justify={{ md: 'center' }}>
-              {Array(3)
-                .fill(null)
-                .map((_, index) => {
-                  const active = index === actualSlide
-                  const bg = isWhite ? 'bgPrimary' : 'white'
-                  return (
-                    <Button
-                      ml={1}
-                      size="xs"
-                      key={index}
-                      rounded="full"
-                      variant="unstyled"
-                      bg={{
-                        base: active ? bg : 'bgSecundary',
-                        md: active ? 'bgPrimary' : 'bgSecundary'
-                      }}
-                      transform={`scale(${active ? 1 : 0.7})`}
-                      onClick={() => updateSlide({ currentSlide: index })}
-                    />
-                  )
-                })}
-            </Flex>
-          </Box>
-        )}
+            {children}
+          </ReactSlider>
+          <Flex mt={5} ml={{ base: '10%', md: 0 }} justify={{ md: 'center' }}>
+            {Array(3)
+              .fill(null)
+              .map((_, index) => {
+                const active = index === actualSlide
+                const bg = isWhite ? 'bgPrimary' : 'white'
+                return (
+                  <Button
+                    ml={1}
+                    size="xs"
+                    key={index}
+                    rounded="full"
+                    variant="unstyled"
+                    bg={{
+                      base: active ? bg : 'bgSecundary',
+                      md: active ? 'bgPrimary' : 'bgSecundary'
+                    }}
+                    transform={`scale(${active ? 1 : 0.7})`}
+                    onClick={() => updateSlide({ currentSlide: index })}
+                  />
+                )
+              })}
+          </Flex>
+        </Box>
       </Flex>
     </Box>
   )
