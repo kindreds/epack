18942ee65aae4afdd2a8c3497fcbf4a6a291eac1
Cristian González <kazragore96@gmail.com>
2021-06-29 10:12:04 -0500

    Analiticas agregadas 2

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,7 @@
-import { ChakraProvider } from '@chakra-ui/react'
+import { useEffect } from 'react'
 import { NextSeo } from 'next-seo'
+import TagManager from 'react-gtm-module'
+import { ChakraProvider } from '@chakra-ui/react'
 
 import theme from '../theme'
 import meta from '../src/meta'
@@ -9,6 +11,10 @@ import AppState from '../src/context/AppState'
 /* eslint-disable */
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    TagManager.initialize({ gtmId: 'GTM-KBLCP3J' })
+  }, [])
+
   return (
     <ChakraProvider theme={theme}>
       <AppState>
--- pages/_document.js
+++ pages/_document.js
@@ -1,19 +1,10 @@
 import React from 'react'
 import Document, { Html, Head, Main, NextScript } from 'next/document'
-import TagManager from 'react-gtm-module'
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
     const initialProps = await Document.getInitialProps(ctx)
     return { ...initialProps }
-  }
-
-  componentDidMount() {
-    const tagManagerArgs = {
-      gtmId: 'GTM-KBLCP3J'
-    }
-
-    TagManager.initialize(tagManagerArgs)
   }
 
   render() {
