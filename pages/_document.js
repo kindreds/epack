0f3d9c0e152c85e0a835a03471ed161ff5ea84bb
Cristian González <kazragore96@gmail.com>
2021-06-29 10:07:16 -0500

    Analiticas agregadas

--- pages/_document.js
+++ pages/_document.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Document, { Html, Head, Main, NextScript } from 'next/document'
+import TagManager from 'react-gtm-module'
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
@@ -7,10 +8,34 @@ class MyDocument extends Document {
     return { ...initialProps }
   }
 
+  componentDidMount() {
+    const tagManagerArgs = {
+      gtmId: 'GTM-KBLCP3J'
+    }
+
+    TagManager.initialize(tagManagerArgs)
+  }
+
   render() {
     return (
       <Html>
-        <Head />
+        <Head>
+          <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=UA-175669111-1"
+          />
+
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', 'UA-175669111-1');
+              `
+            }}
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
