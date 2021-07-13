0a4daecba03d6e915d58cd544c811df1eda6795e
Cristian González <kazragore96@gmail.com>
2021-07-13 17:11:08 -0500

    Agregando imagenes seo 3

--- src/components/Ubicanos/UbicanosDesk.js
+++ src/components/Ubicanos/UbicanosDesk.js
@@ -45,6 +45,34 @@ const UbicanosDesk = () => {
           )}
         </InView>
         <Flex>
+          <Box w={{ base: 300 }} maxW="300px" mb={6}>
+            <InView>
+              {({ ref, inView }) => (
+                <div ref={ref}>
+                  {inView ? (
+                    <AspectRatio ratio={1} mb={4}>
+                      <motion.iframe
+                        initial={{ y: '-100%', opacity: 0 }}
+                        animate={
+                          inView
+                            ? { y: 0, opacity: 1 }
+                            : { y: '-100%', opacity: 0 }
+                        }
+                        loading="lazy"
+                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3901.5380387505425!2d-77.08415948601476!3d-12.075273445717206!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9105c92a8c49324b%3A0x136ab8515df7ce5b!2sePack%20-%20Bolsas%20Ecol%C3%B3gicas%20Kraft!5e0!3m2!1ses!2spe!4v1626149018231!5m2!1ses!2spe"
+                      />
+                    </AspectRatio>
+                  ) : null}
+                </div>
+              )}
+            </InView>
+            <Text textAlign="center" color="bgPrimary" fontWeight="semibold">
+              San Martín de Porres:
+            </Text>
+            <Text textAlign="center" color="bgSecundary" fontWeight="semibold">
+              C.C. Shopping Center Av. La Mar 2275 Int. 77
+            </Text>
+          </Box>
           <Box w={{ base: 300 }} maxW="300px" mb={6} mr={5}>
             <InView>
               {({ ref, inView }) => (
@@ -72,34 +100,6 @@ const UbicanosDesk = () => {
             </Text>
             <Text textAlign="center" color="bgSecundary" fontWeight="semibold">
               Mz. Z Lt. 37 Urb. COOPIP
-            </Text>
-          </Box>
-          <Box w={{ base: 300 }} maxW="300px" mb={6}>
-            <InView>
-              {({ ref, inView }) => (
-                <div ref={ref}>
-                  {inView ? (
-                    <AspectRatio ratio={1} mb={4}>
-                      <motion.iframe
-                        initial={{ y: '-100%', opacity: 0 }}
-                        animate={
-                          inView
-                            ? { y: 0, opacity: 1 }
-                            : { y: '-100%', opacity: 0 }
-                        }
-                        loading="lazy"
-                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3901.5380387505425!2d-77.08415948601476!3d-12.075273445717206!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9105c92a8c49324b%3A0x136ab8515df7ce5b!2sePack%20-%20Bolsas%20Ecol%C3%B3gicas%20Kraft!5e0!3m2!1ses!2spe!4v1626149018231!5m2!1ses!2spe"
-                      />
-                    </AspectRatio>
-                  ) : null}
-                </div>
-              )}
-            </InView>
-            <Text textAlign="center" color="bgPrimary" fontWeight="semibold">
-              San Martín de Porres:
-            </Text>
-            <Text textAlign="center" color="bgSecundary" fontWeight="semibold">
-              C.C. Shopping Center Av. La Mar 2275 Int. 77
             </Text>
           </Box>
         </Flex>
--- src/meta/index.js
+++ src/meta/index.js
@@ -4,18 +4,16 @@ export default {
     'Bolsas Ecológicas de papel Kraft y Liner a medida personalizada con asa o sin asa. Bolsas para ropa, calzado, accesorios, botellas, alimentos, pan y más. Atención 24/7.',
   // canonical: 'https://www.canonical.ie/',
   openGraph: {
+    type: 'website',
     url: 'https://bolsas.epack.pe/',
     title: 'ePack',
     description:
       'Bolsas Ecológicas de papel Kraft y Liner a medida personalizada con asa o sin asa. Bolsas para ropa, calzado, accesorios, botellas, alimentos, pan y más. Atención 24/7.',
     images: [
-      { url: 'https://bolsas.epack.pe/imagenes/epack-logo-wsp.webp' },
-      { url: 'https://bolsas.epack.pe/imagenes/epack-logo-fb.webp' }
+      { url: 'https://bolsas.epack.pe/imagenes/epack-logo-fb.webp' },
+      { url: 'https://bolsas.epack.pe/imagenes/epack-logo-wsp.webp' }
     ],
     site_name: 'https://bolsas.epack.pe/'
-  },
-  facebook: {
-    appId: '330069158766167'
   },
   twitter: {
     site: '@epack.pe',
