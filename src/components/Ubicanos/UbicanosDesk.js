17e62e721b2e54a41ac7d552b2951e643d6f53e0
Cristian González <kazragore96@gmail.com>
2021-07-12 23:28:56 -0500

    Actualizados mapas

--- src/components/Ubicanos/UbicanosDesk.js
+++ src/components/Ubicanos/UbicanosDesk.js
@@ -32,12 +32,11 @@ const UbicanosDesk = () => {
           </Heading>
         </Box>
         <Flex>
-          <Box maxW="300px" mb={6} mr={5}>
+          <Box w={{ base: 300 }} maxW="300px" mb={6} mr={5}>
             <AspectRatio ratio={1} mb={4}>
               <iframe
                 loading="lazy"
-                // style={{ display: isOpen ? 'block' : 'none' }}
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3900.890185810459!2d-77.00020268578656!3d-12.119665546559304!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9105c7f07aacf4bd%3A0xc51e56a54e01242b!2sAv.%20Aviaci%C3%B3n%204557%2C%20Surquillo%2015038!5e0!3m2!1ses!2spe!4v1623368284298!5m2!1ses!2spe"
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3902.7001708113294!2d-77.10080348601603!3d-11.99523644420799!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9105cfe4e266385f%3A0xa6cc44d77672c755!2sEpack%20-%20Bolsas%20Ecol%C3%B3gicas!5e0!3m2!1ses!2spe!4v1626149095298!5m2!1ses!2spe"
               />
             </AspectRatio>
 
@@ -45,19 +44,18 @@ const UbicanosDesk = () => {
               San Miguel:
             </Text>
             <Text textAlign="center" color="bgSecundary" fontWeight="semibold">
-              C.C. Shopping Center Av. La Mar 2275 Int. 77
+              Mz. Z Lt. 37 Urb. COOPIP
             </Text>
           </Box>
-          <Box maxW="300px" mb={6}>
+          <Box w={{ base: 300 }} maxW="300px" mb={6}>
             <AspectRatio ratio={1} mb={4}>
               <iframe
                 loading="lazy"
-                // style={{ display: isOpen ? 'block' : 'none' }}
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3900.890185810459!2d-77.00020268578656!3d-12.119665546559304!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9105c7f07aacf4bd%3A0xc51e56a54e01242b!2sAv.%20Aviaci%C3%B3n%204557%2C%20Surquillo%2015038!5e0!3m2!1ses!2spe!4v1623368284298!5m2!1ses!2spe"
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3901.5380387505425!2d-77.08415948601476!3d-12.075273445717206!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9105c92a8c49324b%3A0x136ab8515df7ce5b!2sePack%20-%20Bolsas%20Ecol%C3%B3gicas%20Kraft!5e0!3m2!1ses!2spe!4v1626149018231!5m2!1ses!2spe"
               />
             </AspectRatio>
             <Text textAlign="center" color="bgPrimary" fontWeight="semibold">
-              San Miguel:
+              San Martín de Porres:
             </Text>
             <Text textAlign="center" color="bgSecundary" fontWeight="semibold">
               C.C. Shopping Center Av. La Mar 2275 Int. 77
