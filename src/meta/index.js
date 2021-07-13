5940e6867c6d3ad22a984cfed548f0467e2af1d5
Cristian González <kazragore96@gmail.com>
2021-07-13 17:02:52 -0500

    Agregadas imagenes para SEO 2

--- src/meta/index.js
+++ src/meta/index.js
@@ -4,25 +4,23 @@ export default {
     'Bolsas Ecológicas de papel Kraft y Liner a medida personalizada con asa o sin asa. Bolsas para ropa, calzado, accesorios, botellas, alimentos, pan y más. Atención 24/7.',
   // canonical: 'https://www.canonical.ie/',
   openGraph: {
-    // url: 'https://www.url.ie/a',
+    url: 'https://bolsas.epack.pe/',
     title: 'ePack',
     description:
       'Bolsas Ecológicas de papel Kraft y Liner a medida personalizada con asa o sin asa. Bolsas para ropa, calzado, accesorios, botellas, alimentos, pan y más. Atención 24/7.',
     images: [
-      {
-        url: 'https://bolsas.epack.pe/imagenes/epack-logo-wsp.webp',
-        width: 400,
-        height: 400,
-        alt: 'Logo ePack whatsapp'
-      },
-      {
-        url: 'https://bolsas.epack.pe/imagenes/epack-logo-fb.webp',
-        width: 560,
-        height: 292,
-        alt: 'Logo ePack facebook'
-      }
+      { url: 'https://bolsas.epack.pe/imagenes/epack-logo-wsp.webp' },
+      { url: 'https://bolsas.epack.pe/imagenes/epack-logo-fb.webp' }
     ],
-    site_name: 'https://www.epack.pe/'
+    site_name: 'https://bolsas.epack.pe/'
+  },
+  facebook: {
+    appId: '330069158766167'
+  },
+  twitter: {
+    site: '@epack.pe',
+    handle: '@handle',
+    cardType: 'summary_large_image'
   },
   additionalLinkTags: [
     {
